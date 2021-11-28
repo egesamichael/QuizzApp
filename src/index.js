cf52b2f16c829972d9d35026abee5dbb027bbd08
Cathella <ncathie214@gmail.com>
2021-11-28 07:13:10 +0300

    Add eventlisteners, selectedoption

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ const _question = document.getElementById('question');
 const _options = document.querySelector('.quiz-options');
 const _correctScore = document.getElementById('correct-score');
 const _totalQuestion = document.getElementById('total-question');
+const _checkBtn = document.getElementById('check-answer');
+const _playAgainBtn = document.getElementById('play-again');
+const _result = document.getElementById('result');
 
 let correctAnswer = "", correctScore = 0, totalQuestion = 15;
+let askedCount = 0;
+
+// event listeners
+function eventListeners() {
+  _checkBtn.addEventListener('click', checkAnswer);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   loadQuestion();
+  eventListeners();
   _totalQuestion.textContent = totalQuestion;
   _correctScore.textContent = correctScore;
 });
@@ -21,6 +31,7 @@ async function loadQuestion() {
   showQuestion(data.results[0]);
 }
 
+// display question and options
 function showQuestion(data) {
   correctAnswer = data.correct_answer;
   let incorrectAnswer = data.incorrect_answers;
@@ -33,4 +44,61 @@ function showQuestion(data) {
       <li>${index + 1}. <span> ${option} </span></li>
     `).join('')}
   `;
+
+  selectOption();
+}
+
+// options selection
+function selectOption() {
+  _options.querySelectorAll('li').forEach((option) => {
+    option.addEventListener('click', () => {
+      if(_options.querySelector('.selected')) {
+        const activeOption = _options.querySelector('.selected');
+        activeOption.classList.remove('selected');
+      }
+
+      option.classList.add('selected');
+    });
+  });
+
+  console.log(correctAnswer);
+}
+
+function checkAnswer() {
+  _checkBtn.disabled = true;
+
+  if(_options.querySelector('.selected')) {
+    let selectedAnswer = _options.querySelector('.selected span').textContent;
+    
+    if(selectedAnswer.trim() == HTMLDecode(correctAnswer)) {
+      correctScore++;
+      _result.innerHTML = `<p><i class="fas fa-check"></i> Correct Answer!</p>`;
+    } else {
+      _result.innerHTML = `<p><i class="fas fa-times"></i> Incorrect Answer!</p><p><small><b>Correct Answer: </b>${correctAnswer}</small></p>`;
+    }
+
+    checkCount();
+  }
+}
+
+function HTMLDecode(textString) {
+  let doc = new DOMParser().parseFromString(textString, "text/html");
+  return doc.documentElement.textContent;
+}
+
+function checkCount() {
+  askedCount++;
+  setCount();
+  if(askedCount == totalQuestion) {
+
+  } else {
+    setTimeout(() => {
+      loadQuestion();
+    }, 300);
+  }
+}
+
+function setCount() {
+  _totalQuestion.textContent = totalQuestion;
+  _correctScore.textContent = correctScore;
 }
