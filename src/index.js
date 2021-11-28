9219f39c2bb203f2ae1bf27a7465bb4492f7c867
Cathella <ncathie214@gmail.com>
2021-11-28 05:46:19 +0300

    Display a question and its options

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// https://opentdb.com/api.php?amount=15
 import './style.css';
 
-console.log("hello there");
+const _question = document.getElementById('question');
+const _options = document.querySelector('.quiz-options');
+const _correctScore = document.getElementById('correct-score');
+const _totalQuestion = document.getElementById('total-question');
+
+let correctAnswer = "", correctScore = 0, totalQuestion = 15;
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadQuestion();
+  _totalQuestion.textContent = totalQuestion;
+  _correctScore.textContent = correctScore;
+});
+
+async function loadQuestion() {
+  const APIUrl = 'https://opentdb.com/api.php?amount=1';
+  const result = await fetch(`${APIUrl}`);
+  const data = await result.json();
+  showQuestion(data.results[0]);
+}
+
+function showQuestion(data) {
+  correctAnswer = data.correct_answer;
+  let incorrectAnswer = data.incorrect_answers;
+  let optionsList = incorrectAnswer;
+  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+
+  _question.innerHTML = `${data.question} <br> <span class="category">${data.category}</span>`;
+  _options.innerHTML = `
+    ${optionsList.map((option, index) => `
+      <li>${index + 1}. <span> ${option} </span></li>
+    `).join('')}
+  `;
+}
