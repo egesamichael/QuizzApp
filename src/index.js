73c24a0efa145aec05d2b7ac4735bda9f257eb1b
Cathella <ncathie214@gmail.com>
2021-11-29 07:28:23 +0300

    Remove stylelint errors

--- src/index.js
+++ src/index.js
@@ -1,58 +1,43 @@
 import './style.css';
 
-const _question = document.getElementById('question');
-const _options = document.querySelector('.quiz-options');
-const _correctScore = document.getElementById('correct-score');
-const _totalQuestion = document.getElementById('total-question');
-const _checkBtn = document.getElementById('check-answer');
-const _playAgainBtn = document.getElementById('play-again');
-const _result = document.getElementById('result');
+const question = document.getElementById('question');
+const options = document.querySelector('.quiz-options');
+const correctScores = document.getElementById('correct-score');
+const totalQn = document.getElementById('total-question');
+const checkBtn = document.getElementById('check-answer');
+const playAgainBtn = document.getElementById('play-again');
+const results = document.getElementById('result');
 
-let correctAnswer = "", correctScore = 0, totalQuestion = 15;
+let correctAnswer = '';
+let correctScore = 0;
+const totalQuestion = 15;
 let askedCount = 0;
 
 const eventListeners = () => {
-  _checkBtn.addEventListener('click', checkAnswer);
-  _playAgainBtn.addEventListener('click', restartQuiz);
+  checkBtn.addEventListener('click', checkAnswer);
+  playAgainBtn.addEventListener('click', restartQuiz);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   loadQuestion();
   eventListeners();
-  _totalQuestion.textContent = totalQuestion;
-  _correctScore.textContent = correctScore;
+  totalQn.textContent = totalQuestion;
+  correctScores.textContent = correctScore;
 });
 
 const loadQuestion = async () => {
   const APIUrl = 'https://opentdb.com/api.php?amount=1';
   const result = await fetch(`${APIUrl}`);
   const data = await result.json();
-  _result.innerHTML = "";
+  results.innerHTML = "";
   showQuestion(data.results[0]);
 }
 
-const showQuestion = (data) => {
-  _checkBtn.disabled = false;
-  correctAnswer = data.correct_answer;
-  let incorrectAnswer = data.incorrect_answers;
-  let optionsList = incorrectAnswer;
-  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-
-  _question.innerHTML = `${data.question} <br> <span class="category">${data.category}</span>`
-  _options.innerHTML = `
-    ${optionsList.map((option, index) => `
-      <li><span> ${option} </span></li>
-    `).join('')}
-  `;
-
-  selectOption();
-}
-
 const selectOption = () => {
-  _options.querySelectorAll('li').forEach((option) => {
+  options.querySelectorAll('li').forEach((option) => {
     option.addEventListener('click', () => {
-      if(_options.querySelector('.selected')) {
-        const activeOption = _options.querySelector('.selected');
+      if(options.querySelector('.selected')) {
+        const activeOption = options.querySelector('.selected');
         activeOption.classList.remove('selected');
       }
 
@@ -61,28 +46,45 @@ const selectOption = () => {
   });
 }
 
-const checkAnswer = () => {
-  _checkBtn.disabled = true;
+const showQuestion = (data) => {
+  checkBtn.disabled = false;
+  correctAnswer = data.correct_answer;
+  const incorrectAnswer = data.incorrect_answers;
+  const optionsList = incorrectAnswer;
+  optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
 
-  if(_options.querySelector('.selected')) {
-    let selectedAnswer = _options.querySelector('.selected span').textContent;
+  question.innerHTML = `${data.question} <br> <span class="category">${data.category}</span>`
+  options.innerHTML = `
+    ${optionsList.map((option) => `
+      <li><span> ${option} </span></li>
+    `).join('')}
+  `;
+
+  selectOption();
+}
+
+const checkAnswer = () => {
+  checkBtn.disabled = true;
+
+  if(options.querySelector('.selected')) {
+    let selectedAnswer = options.querySelector('.selected span').textContent;
     
     if(selectedAnswer.trim() == HTMLDecode(correctAnswer)) {
       correctScore++;
-      _result.innerHTML = `<p><i class="fas fa-check success"></i> Correct Answer!</p>`;
+      results.innerHTML = `<p><i class="fas fa-check success"></i> Correct Answer!</p>`;
     } else {
-      _result.innerHTML = `<p><i class="fas fa-times fail"></i> Incorrect Answer!</p>`;
+      results.innerHTML = `<p><i class="fas fa-times fail"></i> Incorrect Answer!</p>`;
     }
 
     checkCount();
   } else {
-    _result.innerHTML = `<p><i class="fas fa-question"></i> Please select an option!</p>`;
-    _checkBtn.disabled = false;
+    results.innerHTML = `<p><i class="fas fa-question"></i> Please select an option!</p>`;
+    checkBtn.disabled = false;
   }
 }
 
 const HTMLDecode = (textString) => {
-  let doc = new DOMParser().parseFromString(textString, "text/html");
+  let doc = new DOMParser().parseFromString(textString, 'text/html');
   return doc.documentElement.textContent;
 }
 
@@ -90,9 +92,9 @@ const checkCount = () => {
   askedCount++;
   setCount();
   if(askedCount == totalQuestion) {
-    _result.innerHTML += `<p class="completed">Your score is ${correctScore}.</p>`;
-    _playAgainBtn.style.display = "block";
-    _checkBtn.style.display = "none";
+    results.innerHTML += `<p class="completed">Your score is ${correctScore}.</p>`;
+    playAgainBtn.style.display = "block";
+    checkBtn.style.display = "none";
   } else {
     setTimeout(() => {
       loadQuestion();
@@ -101,15 +103,15 @@ const checkCount = () => {
 }
 
 const setCount = () => {
-  _totalQuestion.textContent = totalQuestion;
-  _correctScore.textContent = correctScore;
+  totalQn.textContent = totalQuestion;
+  correctScores.textContent = correctScore;
 }
 
 const restartQuiz = () => {
   correctScore = askedCount = 0;
-  _playAgainBtn.style.display = "none";
-  _checkBtn.style.display = "block";
-  _checkBtn.disabled = false;
+  playAgainBtn.style.display = "none";
+  checkBtn.style.display = "block";
+  checkBtn.disabled = false;
   setCount();
   loadQuestion();
 }
